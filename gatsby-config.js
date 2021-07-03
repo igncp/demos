e6b288ca9c6eaec8275bf5351ea8232ea31cdf9f
Ignacio Carbajo <icarbajop@gmail.com>
2021-07-03 21:55:48 +0200

    Use prefix paths for build

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  pathPrefix: process.env.ROOT_PATH
+    ? process.env.ROOT_PATH.replace(/\/$/, "")
+    : "/",
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
