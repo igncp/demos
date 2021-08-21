710b94c3ddc0cf5ca7741651229ab11a7d487ba2
Ignacio Carbajo <icarbajop@gmail.com>
2021-08-21 13:48:23 +0200

    Add story

---
+++ storybook/stories/ThreeJSPhysicsFalls/OimoPhysics.ts
@@ -0,0 +1,259 @@
+// https://github.com/mrdoob/three.js/blob/dev/examples/jsm/physics/OimoPhysics.js
+import { InstancedMesh, Mesh, Vector3 } from "three"
+// @ts-ignore
+import * as OIMO from "three/examples/jsm/libs/OimoPhysics/index.js"
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+type OIMOBody = any
+type OIMOGeometry = any
+type THREEGeometry = any
+/* eslint-enable @typescript-eslint/no-explicit-any */
+
+type Point = { x: number; y: number; z: number }
+type MeshLike = InstancedMesh | Mesh
+
+type Compose = (o: {
+  arrayBaseIndex: number
+  matrixArray: number[]
+  position: Point
+  quaternion: Point & { w: number }
+}) => void
+
+const compose: Compose = ({
+  arrayBaseIndex,
+  matrixArray,
+  position,
+  quaternion,
+}) => {
+  const { x } = quaternion
+  const { y } = quaternion
+  const { z } = quaternion
+  const { w } = quaternion
+  const x2 = x + x
+  const y2 = y + y
+  const z2 = z + z
+  const xx = x * x2
+  const xy = x * y2
+  const xz = x * z2
+  const yy = y * y2
+  const yz = y * z2
+  const zz = z * z2
+  const wx = w * x2
+  const wy = w * y2
+  const wz = w * z2
+
+  matrixArray[arrayBaseIndex + 0] = 1 - (yy + zz)
+  matrixArray[arrayBaseIndex + 1] = xy + wz
+  matrixArray[arrayBaseIndex + 2] = xz - wy
+  matrixArray[arrayBaseIndex + 3] = 0
+
+  matrixArray[arrayBaseIndex + 4] = xy - wz
+  matrixArray[arrayBaseIndex + 5] = 1 - (xx + zz)
+  matrixArray[arrayBaseIndex + 6] = yz + wx
+  matrixArray[arrayBaseIndex + 7] = 0
+
+  matrixArray[arrayBaseIndex + 8] = xz + wy
+  matrixArray[arrayBaseIndex + 9] = yz - wx
+  matrixArray[arrayBaseIndex + 10] = 1 - (xx + yy)
+  matrixArray[arrayBaseIndex + 11] = 0
+
+  matrixArray[arrayBaseIndex + 12] = position.x
+  matrixArray[arrayBaseIndex + 13] = position.y
+  matrixArray[arrayBaseIndex + 14] = position.z
+  matrixArray[arrayBaseIndex + 15] = 1
+}
+
+const createOimoPhysics = ({ frameRate = 90 } = {}) => {
+  const world = new OIMO.World(2, new OIMO.Vec3(0, -9.8, 0))
+
+  const meshes: MeshLike[] = []
+  const meshMap = new WeakMap()
+
+  type HandleMesh = (o: {
+    mass: number
+    mesh: Mesh
+    oimoGeometry: OIMOGeometry
+  }) => void
+
+  const handleMesh: HandleMesh = ({ mass, mesh, oimoGeometry }) => {
+    const shapeConfig = new OIMO.ShapeConfig()
+
+    shapeConfig.geometry = oimoGeometry
+
+    const bodyConfig = new OIMO.RigidBodyConfig()
+
+    bodyConfig.type =
+      mass === 0 ? OIMO.RigidBodyType.STATIC : OIMO.RigidBodyType.DYNAMIC
+
+    bodyConfig.position = new OIMO.Vec3(
+      mesh.position.x,
+      mesh.position.y,
+      mesh.position.z
+    )
+
+    const body = new OIMO.RigidBody(bodyConfig)
+
+    body.addShape(new OIMO.Shape(shapeConfig))
+    world.addRigidBody(body)
+
+    if (mass > 0) {
+      meshes.push(mesh)
+      meshMap.set(mesh, body)
+    }
+  }
+
+  type HandleInstancedMesh = (o: {
+    mass: number
+    mesh: InstancedMesh
+    oimoGeometry: OIMOGeometry
+  }) => void
+
+  const handleInstancedMesh: HandleInstancedMesh = ({
+    mass,
+    mesh,
+    oimoGeometry,
+  }) => {
+    const {
+      instanceMatrix: { array: matrixArray },
+    } = mesh
+
+    const bodies: OIMOBody[] = []
+
+    Array.from({ length: mesh.count }).forEach((...forEachArgs) => {
+      const [, meshItemIndex] = forEachArgs
+      const arrayIndex = meshItemIndex * 16
+
+      const shapeConfig = new OIMO.ShapeConfig()
+
+      shapeConfig.geometry = oimoGeometry
+
+      const bodyConfig = new OIMO.RigidBodyConfig()
+
+      bodyConfig.type =
+        mass === 0 ? OIMO.RigidBodyType.STATIC : OIMO.RigidBodyType.DYNAMIC
+      bodyConfig.position = new OIMO.Vec3(
+        matrixArray[arrayIndex + 12],
+        matrixArray[arrayIndex + 13],
+        matrixArray[arrayIndex + 14]
+      )
+
+      const body = new OIMO.RigidBody(bodyConfig)
+
+      body.addShape(new OIMO.Shape(shapeConfig))
+      world.addRigidBody(body)
+
+      bodies.push(body)
+    })
+
+    if (mass > 0) {
+      meshes.push(mesh)
+      meshMap.set(mesh, bodies)
+    }
+  }
+
+  const getOIMOGeometry = (geometry: THREEGeometry): OIMOGeometry => {
+    const { parameters } = geometry
+
+    if (geometry.type === "BoxGeometry") {
+      const sx = parameters.width !== undefined ? parameters.width / 2 : 0.5
+      const sy = parameters.height !== undefined ? parameters.height / 2 : 0.5
+      const sz = parameters.depth !== undefined ? parameters.depth / 2 : 0.5
+
+      return new OIMO.OBoxGeometry(new OIMO.Vec3(sx, sy, sz))
+    }
+
+    const radius = parameters.radius !== undefined ? parameters.radius : 1
+
+    return new OIMO.OSphereGeometry(radius)
+  }
+
+  type AddMesh = (o: { mass?: number; mesh: MeshLike }) => void
+
+  const addMesh: AddMesh = ({ mass = 0, mesh }) => {
+    const oimoGeometry = getOIMOGeometry(mesh.geometry)
+
+    if (oimoGeometry !== null) {
+      if ("isInstancedMesh" in mesh) {
+        handleInstancedMesh({ mass, mesh, oimoGeometry })
+      } else if ("isMesh" in mesh) {
+        handleMesh({ mass, mesh, oimoGeometry })
+      }
+    }
+  }
+
+  type SetMeshPosition = (o: {
+    bodyIndex?: number
+    mesh: MeshLike
+    position: Vector3
+  }) => void
+
+  const setMeshPosition: SetMeshPosition = ({
+    bodyIndex = 0,
+    mesh,
+    position,
+  }) => {
+    if ("isInstancedMesh" in mesh) {
+      const bodies = meshMap.get(mesh)
+      const { [bodyIndex]: body } = bodies
+
+      body.setPosition(new OIMO.Vec3(position.x, position.y, position.z))
+    } else if ("isMesh" in mesh) {
+      const body = meshMap.get(mesh)
+
+      body.setPosition(new OIMO.Vec3(position.x, position.y, position.z))
+    }
+  }
+
+  let lastTime = 0
+
+  const step = () => {
+    const time = performance.now()
+
+    if (lastTime > 0) {
+      world.step(1 / frameRate)
+    }
+
+    lastTime = time
+
+    meshes.forEach((mesh) => {
+      if ("isInstancedMesh" in mesh) {
+        const matrixArray = mesh.instanceMatrix.array as number[]
+        const bodies: OIMOBody[] = meshMap.get(mesh)
+
+        bodies.forEach((...forEachArgs) => {
+          const [body, bodyIndex] = forEachArgs
+
+          compose({
+            arrayBaseIndex: bodyIndex * 16,
+            matrixArray,
+            position: body.getPosition(),
+            quaternion: body.getOrientation(),
+          })
+        })
+
+        mesh.instanceMatrix.needsUpdate = true
+      } else if ("isMesh" in mesh) {
+        const body = meshMap.get(mesh)
+
+        mesh.position.copy(body.getPosition())
+        mesh.quaternion.copy(body.getOrientation())
+      }
+    })
+  }
+
+  const intervalId = setInterval(() => {
+    requestAnimationFrame(step)
+  }, 1000 / frameRate)
+
+  const stop = () => {
+    clearInterval(intervalId)
+  }
+
+  return {
+    addMesh,
+    setMeshPosition,
+    stop,
+  }
+}
+
+export { createOimoPhysics }
---
+++ storybook/stories/ThreeJSPhysicsFalls/ThreeJSPhysicsFalls.stories.tsx
@@ -0,0 +1,329 @@
+import { scaleLinear } from "d3"
+import debounce from "lodash/debounce"
+import React from "react"
+import {
+  BoxGeometry,
+  Color,
+  DirectionalLight,
+  DynamicDrawUsage,
+  HemisphereLight,
+  IcosahedronGeometry,
+  InstancedMesh,
+  Matrix4,
+  Mesh,
+  MeshLambertMaterial,
+  PerspectiveCamera,
+  Scene,
+  ShadowMaterial,
+  Vector3,
+  WebGLRenderer,
+  sRGBEncoding,
+} from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+
+import { StoryInfo, TemplateType, createRangeControl } from "../common"
+
+import { createOimoPhysics } from "./OimoPhysics"
+
+const ROOT_ID = "example"
+
+type Physics = ReturnType<typeof createOimoPhysics>
+
+type State = {
+  isStopped: boolean
+  timeCounter: number
+}
+
+type SimulationEls = {
+  boxes: InstancedMesh
+  camera: PerspectiveCamera
+  floor: Mesh
+  hemiLight: HemisphereLight
+  position: Vector3
+  renderer: WebGLRenderer
+  scene: Scene
+  spheres: InstancedMesh
+}
+
+type Props = {
+  itemsNumber: number
+  speed: number
+}
+
+type Simulation = {
+  elements: SimulationEls
+  physics: Physics
+  props: Props
+  state: State
+  stop: () => void
+}
+
+const createPhysics = (previousSimulation: Simulation | null): Physics =>
+  previousSimulation?.physics ? previousSimulation.physics : createOimoPhysics()
+
+const speedScale = scaleLinear().domain([0, 200]).range([200, 1])
+
+type CreateDemo = (o: {
+  physics: Physics
+  previousSimulation: Simulation | null
+  props: Props
+}) => Simulation
+
+const createDemo: CreateDemo = ({
+  physics,
+  previousSimulation,
+  props,
+}): Simulation => {
+  const state: State = {
+    timeCounter: 0,
+    ...(previousSimulation?.state ?? {}),
+    isStopped: false,
+  }
+  const container = document.getElementById(ROOT_ID) as HTMLElement
+  const { width } = container.getBoundingClientRect()
+  const height = 500
+
+  const elements: SimulationEls = (() => {
+    if (previousSimulation) {
+      return previousSimulation.elements
+    }
+
+    const floor = new Mesh(
+      new BoxGeometry(1000, 5, 1000),
+      new ShadowMaterial({ color: 0x777777 })
+    )
+
+    const material = new MeshLambertMaterial()
+    const geometryBox = new BoxGeometry(0.1, 0.1, 0.1)
+    const geometrySphere = new IcosahedronGeometry(0.075, 3)
+
+    const shapesTypes = 2
+
+    return {
+      boxes: new InstancedMesh(
+        geometryBox,
+        material,
+        Math.ceil(props.itemsNumber / shapesTypes)
+      ),
+      camera: new PerspectiveCamera(50, width / height, 0.1, 100),
+      floor,
+      hemiLight: new HemisphereLight(),
+      position: new Vector3(),
+      renderer: new WebGLRenderer({ antialias: true }),
+      scene: new Scene(),
+      spheres: new InstancedMesh(
+        geometrySphere,
+        material,
+        Math.ceil(props.itemsNumber / shapesTypes)
+      ),
+    }
+  })()
+
+  const {
+    boxes,
+    camera,
+    floor,
+    hemiLight,
+    position,
+    renderer,
+    scene,
+    spheres,
+  } = elements
+
+  const createSimulation = (): Simulation => ({
+    elements,
+    physics,
+    props,
+    state,
+    stop: () => {
+      state.isStopped = true
+    },
+  })
+
+  const meshList = [boxes, spheres] as const
+
+  const setupInitialSimulation = () => {
+    camera.position.set(-1, 1.5, 2)
+    camera.lookAt(0, 0.5, 0)
+
+    scene.background = new Color(0xffffff)
+
+    hemiLight.intensity = 0.35
+    scene.add(hemiLight)
+
+    const dirLight = new DirectionalLight()
+
+    dirLight.position.set(5, 5, 5)
+    dirLight.castShadow = true
+    dirLight.shadow.camera.zoom = 2
+    scene.add(dirLight)
+
+    floor.position.y = -2.5
+    floor.receiveShadow = true
+    scene.add(floor)
+    physics.addMesh({ mesh: floor })
+
+    meshList.forEach((mesh) => {
+      mesh.instanceMatrix.setUsage(DynamicDrawUsage)
+      mesh.castShadow = true
+      mesh.receiveShadow = true
+      scene.add(mesh)
+
+      const matrix = new Matrix4()
+      const color = new Color()
+
+      Array.from({ length: mesh.count }).forEach((...forEachArgs) => {
+        const [, meshItemIndex] = forEachArgs
+
+        matrix.setPosition(
+          Math.random() - 0.5,
+          Math.random() * 2,
+          Math.random() - 0.5
+        )
+
+        mesh.setMatrixAt(meshItemIndex, matrix)
+        mesh.setColorAt(meshItemIndex, color.setHex(0xffffff * Math.random()))
+      })
+
+      physics.addMesh({ mass: 1, mesh })
+    })
+
+    renderer.setPixelRatio(window.devicePixelRatio)
+    renderer.setSize(width, height)
+    renderer.shadowMap.enabled = true
+    renderer.outputEncoding = sRGBEncoding
+
+    container.innerHTML = ""
+    container.append(renderer.domElement)
+
+    const controls = new OrbitControls(camera, renderer.domElement)
+
+    controls.target.y = 0.5
+    controls.update()
+  }
+
+  const animate = () => {
+    if (state.isStopped) {
+      return
+    }
+
+    requestAnimationFrame(animate)
+
+    renderer.render(scene, camera)
+
+    state.timeCounter += 1
+
+    if (!props.speed) {
+      return
+    }
+
+    const realSpeed = speedScale(props.speed)
+    const diff = Math.floor(state.timeCounter % realSpeed)
+
+    if (diff !== 0) {
+      return
+    }
+
+    meshList
+      .filter(() => Math.random() > 0.5)
+      .forEach((mesh) => {
+        const bodyIndex = Math.floor(Math.random() * mesh.count)
+
+        position.set(0, Math.random() + 1, 0)
+        physics.setMeshPosition({ bodyIndex, mesh, position })
+      })
+  }
+
+  if (previousSimulation) {
+    previousSimulation.stop()
+
+    animate()
+
+    return createSimulation()
+  }
+
+  setupInitialSimulation()
+
+  animate()
+
+  return createSimulation()
+}
+
+type Demo = (o: {
+  previousSimulation: Simulation | null
+  props: Props
+}) => Simulation
+
+const demo: Demo = ({ previousSimulation, props }) => {
+  if (previousSimulation?.props.itemsNumber !== props.itemsNumber) {
+    previousSimulation?.physics.stop()
+
+    const physics = createPhysics(null)
+
+    return createDemo({ physics, previousSimulation: null, props })
+  }
+
+  const physics = createPhysics(previousSimulation)
+
+  return createDemo({ physics, previousSimulation, props })
+}
+
+const ThreeJSPhysicsFalls = (props: Props) => {
+  const simulationRef = React.useRef<Simulation | null>(null)
+
+  const runSimulation = React.useCallback(
+    debounce((latestProps: Props) => {
+      simulationRef.current?.stop()
+
+      simulationRef.current = demo({
+        previousSimulation: simulationRef.current,
+        props: latestProps,
+      })
+    }, 200),
+    []
+  )
+
+  React.useEffect(() => runSimulation(props), [props])
+
+  return (
+    <div>
+      <StoryInfo
+        docs={[]}
+        source="https://github.com/mrdoob/three.js/blob/dev/examples/physics_oimo_instancing.html"
+        sourceText="Source (official example, ported to TS)"
+        storyName="ThreeJSPhysicsFalls"
+      />
+      <div id={ROOT_ID} />
+    </div>
+  )
+}
+
+const Template = ((props: Props) => (
+  <ThreeJSPhysicsFalls {...props} />
+)) as TemplateType<Props>
+
+export const Common = Template.bind({})
+
+const [speedArgs, speedControls] = createRangeControl(100, 1, 100)
+const [itemsNumberArgs, itemsNumberControls] = createRangeControl(
+  100,
+  1,
+  99,
+  900
+)
+
+const args: Props = {
+  itemsNumber: itemsNumberArgs,
+  speed: speedArgs,
+}
+
+Common.args = args
+
+export default {
+  argTypes: {
+    itemsNumber: itemsNumberControls,
+    speed: speedControls,
+  },
+  component: ThreeJSPhysicsFalls,
+  title: "ThreeJS/Physics Falls",
+}
