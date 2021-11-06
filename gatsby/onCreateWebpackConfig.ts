485ccf0bdf7682d1b323a44b2163cdff4a7f429d
Ignacio Carbajo <icarbajop@gmail.com>
2021-11-06 19:04:37 +0100

    Fix css modules

--- gatsby/onCreateWebpackConfig.ts
+++ gatsby/onCreateWebpackConfig.ts
@@ -1,11 +1,43 @@
-import { GatsbyNode } from "gatsby"
+import { CreateWebpackConfigArgs, GatsbyNode } from "gatsby"
 
 import { ROOT_PATH } from "./constants"
 
-const onCreateWebpackConfig: GatsbyNode["onCreateWebpackConfig"] = ({
-  actions,
-  plugins,
-}) => {
+// This is required after update to have valid CSS selectors for `.module.css` files, so classes don't have symbols
+// like `+`, just numbers and letters
+const fixCSSModules = ({ actions, getConfig }: CreateWebpackConfigArgs) => {
+  const config = getConfig()
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  type Rule = any
+
+  config.module.rules.forEach((topRule: Rule) => {
+    const areCssRules = topRule.oneOf?.some((rule: Rule) =>
+      rule.test.test("style.css")
+    )
+
+    if (!areCssRules) {
+      return
+    }
+
+    topRule.oneOf.forEach((rule: Rule) => {
+      rule.use.forEach((innerRule: Rule) => {
+        if (!innerRule.options?.modules?.localIdentName) {
+          return
+        }
+
+        // By default it is `base64` instead of `hex` which can contain invalid symbols
+        innerRule.options.modules.localIdentName =
+          "[name]--[local]--[hash:hex:5]"
+      })
+    })
+  })
+
+  actions.replaceWebpackConfig(config)
+}
+
+const onCreateWebpackConfig: GatsbyNode["onCreateWebpackConfig"] = (params) => {
+  const { actions, plugins } = params
+
   actions.setWebpackConfig({
     plugins: [
       plugins.define({
@@ -13,6 +45,8 @@ const onCreateWebpackConfig: GatsbyNode["onCreateWebpackConfig"] = ({
       }),
     ],
   })
+
+  fixCSSModules(params)
 }
 
 export { onCreateWebpackConfig }
